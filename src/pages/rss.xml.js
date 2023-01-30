ecf9d33bb6525f020b8dfb29d18822f1d2ad8e77
mzaini30 <muhzaini30@gmail.com>
2023-01-30 15:38:17 +0800

    Termux

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,17 @@
+import rss from '@astrojs/rss';
+
+export async function get(context) {
+	const postImportResult = import.meta.glob('./post/**/*.mdx', { eager: true }); 
+	const posts = Object.values(postImportResult);
+  return rss({
+    title: 'Zen',
+    description: 'Website pemrograman',
+    site: context.site,
+    items: posts.map(x => ({
+    	link: x.url,
+    	title: x.frontmatter.judul,
+    	pubDate: x.frontmatter.tanggal,
+    	description: x.frontmatter.ringkasan
+    }))
+  });
+}
